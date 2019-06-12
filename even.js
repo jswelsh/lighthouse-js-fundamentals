30498f06d031402970cc77eeccae57d0c1b9558f
jswelsh <james.sinclair.welsh@gmail.com>
2019-06-12 04:29:12 +0000

    new even

---
+++ even.js
@@ -0,0 +1,19 @@
+/*const isEven  = function (num) {
+  return num % 2 === 0;
+}
+
+const tenIsEven = isEven(10);
+const elevenIsEven = isEven(11);
+
+console.log(tenIsEven);
+console.log(elevenIsEven);
+*/
+
+const isEven  = function (num) {
+  return num % 2 === 0;
+}
+let six = 6;
+console.log(isEven(10));
+console.log(isEven(11));
+console.log(isEven(5));
+console.log(isEven(six));
