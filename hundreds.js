dfce8c86e4b2ed2be1fd135555d9c1e9c3908ccf
jswelsh <james.sinclair.welsh@gmail.com>
2019-06-12 05:17:47 +0000

    adding hundred to the mix

---
+++ hundreds.js
@@ -0,0 +1,13 @@
+let howManyHundreds = function (num){
+  num -= (num % 100);
+  num /= 100;
+  return num;
+}
+
+
+
+console.log(howManyHundreds(1000), "=?", 10);
+console.log(howManyHundreds(894), "=?", 8);
+console.log(howManyHundreds(520), "=?", 5);
+console.log(howManyHundreds(99), "=?", 0);
+console.log(howManyHundreds(0), "=?", 0);
